04de794fa64849d9e704f28ed8fc0f53e57eb064
gwon <rnjsguq123@gmail.com>
2023-08-21 09:47:47 +0900

    질문페이지 이동문제 해결
    Controller에 loginCheck 넣어줌

--- Project_Rootrip/src/main/java/com/rootrip/rootripteam/question/QuestionController.java
+++ Project_Rootrip/src/main/java/com/rootrip/rootripteam/question/QuestionController.java
@@ -2,15 +2,22 @@ package com.rootrip.rootripteam.question;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.rootrip.rootripteam.member.MemberDAO;
+
 @Controller
 public class QuestionController {
 	
+	@Autowired
+	private MemberDAO mDAO;
+	
 	@RequestMapping(value = "/question.question.go", method = RequestMethod.GET)
 	public String goQuestion(HttpServletRequest req) {
+		mDAO.loginCheck(req);
 		req.setAttribute("contentPage", "question/question4.jsp");
 		return "index";
 	}
